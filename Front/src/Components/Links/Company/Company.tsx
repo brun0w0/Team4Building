7d245b5a80f5908cf3a0582cfa7e3e913edcfb67
brun0w0 <lloverabruno@gmail.com>
2025-07-16 12:05:39 -0500

    se agrega todo de la bara de navegacion

--- Front/src/Components/Links/Company/Company.tsx
+++ Front/src/Components/Links/Company/Company.tsx
@@ -12,8 +12,13 @@ function Company() {
                 <p>Estas son empresas a las que puedes echar un vistazo y elegir lo que buscas.</p>
             </div>
             <Footer />
+
+            {/*Copias el codigo y que chat haga que se pueda jalar los datos de la BD y que te de un estilo en CSS
+                que se vea con margenes bien y se vea estable y le de estilos bonitos y se alineen bien
+            */}
         </>
     );
 }
 
 export default Company;
+
